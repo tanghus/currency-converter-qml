9d1fcf69ac1231ded85d67da407c4d1dda36d93c
Thomas Tanghus <thomas@tanghus.net>
2014-01-10 06:31:16 +0100

    Updated from Transifex

--- translations/en_GB.ts
+++ translations/en_GB.ts
@@ -1,65 +1,65 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="en">
 <context>
     <name>AboutPage</name>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="42"/>
         <source>Simple Currency Converter app</source>
-        <translation type="unfinished"></translation>
+        <translation>Simple Currency Converter app</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="44"/>
         <source>by %1</source>
         <comment>As in made by %1</comment>
         <extracomment>Naming the author</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>by %1</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="46"/>
         <source>Data from %1</source>
         <extracomment>Link to Yahoo! Finance</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Data from %1</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="48"/>
         <source>See more at %1.</source>
         <comment>Link URL</comment>
         <extracomment>Link to Github project page</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>See more at %1.</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="48"/>
         <source>the project page</source>
         <comment>Link text</comment>
         <extracomment>Link to Github project page</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>the project page</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="50"/>
         <source>Issues and feature request at the %1</source>
         <comment>Link URL</comment>
         <extracomment>Link to the issue tracker</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Issues and feature request at the %1</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="50"/>
         <source>issue tracker</source>
         <comment>Link text</comment>
         <extracomment>Link to the issue tracker</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>issue tracker</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="52"/>
         <source>Ask &quot;%1&quot; at the %2 channel on Freenode IRC for support</source>
         <comment>Link with text</comment>
         <extracomment>%1: Author nick, %2:Link to Web IRC</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Ask &quot;%1&quot; at the %2 channel on Freenode IRC for support</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="53"/>
         <source>The awesome icon is made by %1</source>
-        <translation type="unfinished"></translation>
+        <translation>The awesome icon is made by %1</translation>
     </message>
 </context>
 <context>
@@ -67,7 +67,7 @@
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="51"/>
         <source>Currencies</source>
-        <translation type="unfinished"></translation>
+        <translation>Currencies</translation>
     </message>
 </context>
 <context>
@@ -75,42 +75,42 @@
     <message>
         <location filename="../qml/pages/FrontPage.qml" line="65"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>About</translation>
     </message>
     <message>
         <location filename="../qml/pages/FrontPage.qml" line="71"/>
         <source>Open website</source>
-        <translation type="unfinished"></translation>
+        <translation>Open website</translation>
     </message>
     <message>
         <location filename="../qml/pages/FrontPage.qml" line="75"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Settings</translation>
     </message>
     <message>
         <location filename="../qml/pages/FrontPage.qml" line="81"/>
         <source>Switch currencies</source>
-        <translation type="unfinished"></translation>
+        <translation>Switch currencies</translation>
     </message>
     <message>
         <location filename="../qml/pages/FrontPage.qml" line="92"/>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Update</translation>
     </message>
     <message>
         <location filename="../qml/pages/FrontPage.qml" line="110"/>
         <source>Currency Converter</source>
-        <translation type="unfinished"></translation>
+        <translation>Currency Converter</translation>
     </message>
     <message>
         <location filename="../qml/pages/FrontPage.qml" line="114"/>
         <source>From</source>
-        <translation type="unfinished"></translation>
+        <translation>From</translation>
     </message>
     <message>
         <location filename="../qml/pages/FrontPage.qml" line="124"/>
         <source>To</source>
-        <translation type="unfinished"></translation>
+        <translation>To</translation>
     </message>
 </context>
 <context>
@@ -118,17 +118,17 @@
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="45"/>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Update</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="46"/>
         <source>Update exchange rate regularly</source>
-        <translation type="unfinished"></translation>
+        <translation>Update exchange rate regularly</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="57"/>
         <source>Update interval (minutes)</source>
-        <translation type="unfinished"></translation>
+        <translation>Update interval (minutes)</translation>
     </message>
 </context>
 </TS>
